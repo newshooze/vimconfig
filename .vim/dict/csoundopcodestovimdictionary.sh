788ae0dae49a3f077657dcaaf74ba0daa5af9225
newshooze <vincentz337@gmail.com>
2015-11-04 13:02:00 -0500

    Delete csoundopcodestovimdictionary.sh

--- .vim/dict/csoundopcodestovimdictionary.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# This creates a csound dictionary for use in vim
-BLANKLINE=$( csound --list-opcodes 2<&1 | grep -n -G "^$" | awk -F : '{print $1}' )
-echo "$BLANKLINE"
-csound --list-opcodes 2<&1 | tail -n +$BLANKLINE | grep -R -o -h -P "[a-zA-Z_0-9]*" > csd 
-
